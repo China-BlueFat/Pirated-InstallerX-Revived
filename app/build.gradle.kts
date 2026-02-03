4d9d07da585b76f97b919789471743a74443c659
China-BlueFat <991617412@qq.com>
2026-02-03 09:46:06 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // If you use InstallerX source code, package it into apk or other installation package format
         // Please change the applicationId to one that does not conflict with any official release.
         applicationId = project.findProperty("APP_ID") as String?
-            ?: "com.installer.x.revived"
-        namespace = "com.miui.packageinstaller"
+            ?: "com.rosan.installer.x.revived"
+        namespace = "com.rosan.installer"
         minSdk = 26
         targetSdk = 36
         // Version control
